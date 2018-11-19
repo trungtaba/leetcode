6d3a4d4c8b14f3e34668a384a026715aa422e834
TaBa Trung <trungtaba@gmail.com>
2018-11-19 11:31:59 +0700

    two sum

--- Two Sum/Solution.java
+++ Two Sum/Solution.java
@@ -6,10 +6,16 @@ import java.util.HashMap;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer,Integer>map=new HashMap<>();
+        int[]result=new int[2];
         for(int i=0;i<nums.length;i++){
             if(map.containsKey(nums[i])){
-                return [1];
+                result[0]=map.get(nums[1]);
+                result[1]=i;
+                return result;
+            }else{
+                map.put(target-nums[i], i);
             }
         }
+        return result;
     }
 }
