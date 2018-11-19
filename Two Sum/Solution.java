910a14e5c6080126dfe1bacdbb444992431fbcc6
TaBa Trung <trungtaba@gmail.com>
2018-11-19 11:32:32 +0700

    two sum

--- Two Sum/Solution.java
+++ Two Sum/Solution.java
@@ -9,7 +9,7 @@ class Solution {
         int[]result=new int[2];
         for(int i=0;i<nums.length;i++){
             if(map.containsKey(nums[i])){
-                result[0]=map.get(nums[1]);
+                result[0]=map.get(nums[i]);
                 result[1]=i;
                 return result;
             }else{
