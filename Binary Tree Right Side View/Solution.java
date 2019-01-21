c6a695fdb48a68c58368a610a0528a31d11efa88
TaBa Trung <trungtaba@gmail.com>
2019-01-21 16:28:31 +0700

    Binary Tree Right Side View

--- Binary Tree Right Side View/Solution.java
+++ Binary Tree Right Side View/Solution.java
@@ -25,6 +25,7 @@ class TreeNode {
  *     TreeNode(int x) { val = x; }
  * }
  */
+/*
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
         if(root==null){
@@ -53,5 +54,26 @@ class Solution {
             }
         }
         return list;
+    }
+}
+*/
+public class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+        rightView(root, result, 0);
+        return result;
+    }
+    
+    public void rightView(TreeNode curr, List<Integer> result, int currDepth){
+        if(curr == null){
+            return;
+        }
+        if(currDepth == result.size()){
+            result.add(curr.val);
+        }
+        
+        rightView(curr.right, result, currDepth + 1);
+        rightView(curr.left, result, currDepth + 1);
+        
     }
 }
